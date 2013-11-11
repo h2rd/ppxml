3583326413b2cdd8697cd799162a165cd7d03076
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2013-11-12 00:15:25 +0200

    pxml

--- pxml/__init__.py
+++ pxml/__init__.py
@@ -19,6 +19,13 @@ def color_code(code):
   return highlight(code, XmlLexer(), TerminalFormatter())
 
 def main():
+  parser = argparse.ArgumentParser(description="Command-line tool to validate and pretty-print JSON and XML")
+  parser.add_argument("-p", action="store_true", help="XPATH")
+  args = parser.parse_args()
+
+
+  print args
+
   data=sys.stdin.read()
   data=color_code(format_code(data))
 
