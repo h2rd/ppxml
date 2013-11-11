05f37b396dbfafac1ab5ec25a777e5067a00e2c4
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2013-11-12 00:53:56 +0200

    support curl -i param

--- pxml/__init__.py
+++ pxml/__init__.py
@@ -7,27 +7,28 @@ from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexers import XmlLexer
 
-import xml.dom.minidom as xml
-import argparse
+from xml.dom.minidom import parseString
 
 INDENT=' '*2
 
 def format_code(data):
-  return xml.parseString(data).toprettyxml(indent=INDENT)
+  body = ''
+  if data.startswith('HTTP'):
+    end  = data.find("\r\n\r\n")
+    body = data[0:end]
+    data = data[end:].strip()
+
+  lines = [line for line in parseString(data).toprettyxml(indent=INDENT).split('\n')
+           if line.strip()]
+
+  return "%s\n\n%s" % (body, '\n'.join(lines),)
 
 def color_code(code):
   return highlight(code, XmlLexer(), TerminalFormatter())
 
 def main():
-  parser = argparse.ArgumentParser(description="Command-line tool to validate and pretty-print JSON and XML")
-  parser.add_argument("-p", action="store_true", help="XPATH")
-  args = parser.parse_args()
-
-
-  print args
-
-  data=sys.stdin.read()
-  data=color_code(format_code(data))
+  data = sys.stdin.read()
+  data = color_code(format_code(data))
 
   return data
 
