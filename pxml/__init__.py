0d4684dc69d2ca24e3a9d4993bed4a68e945a3ac
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-01-05 18:33:59 +0200

    new feature

--- pxml/__init__.py
+++ pxml/__init__.py
@@ -2,39 +2,48 @@
 # unicode: utf-8
 
 import sys
+import argparse
 
 from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexers import XmlLexer
 
-from xml.dom.minidom import parseString
+from lxml import etree
+import chardet
 
-INDENT=' '*2
 
-def format_code(data):
-  body = ''
-  if data.startswith('HTTP'):
-    end  = data.find("\r\n\r\n")
-    body = data[0:end]
-    data = data[end:].strip()
+def format_code(xml):
+    lines = [line for line in str(xml).split('\n') if line.strip()]
+    return '\n'.join(lines)
 
-  lines = [line for line in parseString(data).toprettyxml(indent=INDENT).split('\n')
-           if line.strip()]
-
-  return "%s\n\n%s" % (body, '\n'.join(lines),)
 
 def color_code(code):
-  return highlight(code, XmlLexer(), TerminalFormatter())
+    return highlight(code, XmlLexer(), TerminalFormatter())
+
+
+def get_xml(xml, xpath=None):
+    parser = etree.XMLParser(remove_blank_text=True, encoding=chardet.detect(xml)['encoding'])
+    root = etree.fromstring(xml, parser)
+    if xpath:
+        return "\n".join([etree.tostring(i, pretty_print=True)
+                          for i in root.xpath(xpath)])
+
+    return etree.tostring(root, pretty_print=True)
+
 
 def main():
-  data = sys.stdin.read()
-  if sys.stdout.isatty():
-    try:
-      data = color_code(format_code(data))
-    except ValueError as e:
-      print e
+    parser = argparse.ArgumentParser(description="Command-line tool to pretty-print XML with xpath")
+    parser.add_argument("--xpath", help="Use xpath")
+    args = parser.parse_args()
 
-  return data
+    data = sys.stdin.read()
+    if sys.stdout.isatty():
+        try:
+            data = color_code(format_code(get_xml(data, args.xpath)))
+        except ValueError as e:
+            print e
+
+    return data or ''
 
 if __name__ == '__main__':
-  print main()
+    print main()
