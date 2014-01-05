532eefd22463fc5e949acc72886ec8f54f6ac9b0
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-01-05 18:33:45 +0200

    add lxml to depends

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-import os
-
 from setuptools import setup, find_packages
 
 setup(
     name='pxml',
     author='Igor Skrynkovskyy',
     author_email='skrynkovskyy@gmail.com',
-    description=('Command-line tool to validate and pretty-print XML.'),
+    description=('Command-line tool pretty-print XML with xpath'),
     license='MIT',
     keywords='xml',
     url='https://github.com/h2rd/pxml',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     package_data = {
         '': ['*.md', 'LICENSE'],
@@ -19,6 +17,7 @@ setup(
         'console_scripts': ['pxml = pxml:main']
     },
     install_requires=[
-        'Pygments==1.6'
+        'Pygments==1.6',
+        'lxml==3.2.5'
     ]
 )
